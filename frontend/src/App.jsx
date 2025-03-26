90a47e83a275d031e1d7c83f02786cf68c549e5f
MattiaEsposito98 <esposito.matty@hotmail.it>
2025-03-26 16:22:27 +0100

    add script nel package json

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,12 @@
+import axios from 'axios'
 function App() {
 
+  const fetchItem = () => {
+    axios.get(`http://localhost:3000/item`)
+      .then(res => {
+        console.log(res)
+      })
+  }
   return (
     <>
 
